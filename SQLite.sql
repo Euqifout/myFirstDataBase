8799b6457c59080e77d63f41fcf08bdfc8c7a62a
Euqifout <165257449+Euqifout@users.noreply.github.com>
2024-08-07 13:31:00 +0200

    Database adjustments

--- SQLite.sql
+++ SQLite.sql
@@ -14,7 +14,7 @@ CREATE TABLE "Books" (
 	"Book_ID"	INTEGER NOT NULL UNIQUE,
 	"Title"	VARCHAR(50),
 	"Author"	VARCHAR(50),
-	"Description"	VARCHAR(50),
+	"Description"	VARCHAR(255),
 	"An_Author"	INTEGER NOT NULL,
 	PRIMARY KEY("Book_ID" AUTOINCREMENT),
 	FOREIGN KEY("An_Author") REFERENCES "Authors"("author_Id")
@@ -28,25 +28,26 @@ INSERT INTO "Authors" ("author_Id", "F_Name", "L_Name", "Nationality") VALUES
 (4, 'Nkateko', 'Green', 'Congo'),
 (5, 'Dineo', 'CK', 'Malawi');
 
--- Populated the books table with test data
-INSERT INTO "Books" ("Book_ID", "Title", "Author", "Description", "An_Author") VALUES
-(1, 'Book One', 'Lerato Kgwedi', 'Description One', 1),
-(2, 'Book Two', 'Toufique Mahlangu', 'Description Two', 2),
-(3, 'Book Three', 'Oarabile Brown', 'Description Three', 3),
-(4, 'Book Four', 'Nkateko Green', 'Description Four', 4),
-(5, 'Book Five', 'Dineo CK', 'Description Five', 5),
-(6, 'Book Six', 'Lerato Kgwedi', 'Description Six', 1),
-(7, 'Book Seven', 'Toufique Mahlangu', 'Description Seven', 2),
-(8, 'Book Eight', 'Oarabile Brown', 'Description Eight', 3),
-(9, 'Book Nine', 'Nkateko Green', 'Description Nine', 4),
-(10, 'Book Ten', 'Dineo CK', 'Description Ten', 5),
-(11, 'Book Eleven', 'Lerato Kgwedi', 'Description Eleven', 1),
-(12, 'Book Twelve', 'Toufique Mahlangu', 'Description Twelve', 2),
-(13, 'Book Thirteen', 'Oarabile Brown', 'Description Thirteen', 3), 
-(14, 'Book Fourteen', 'Nkateko Green', 'Description Fourteen', 4),
-(15, 'Book Fifteen', 'Toufique Mahlangu', 'Description Fifteen', 2);
 
--- Counting the number of rows, books in the books table.
+-- Populated the books table with test data
+INSERT INTO "Books" ("Title", "Author", "Description", "An_Author") VALUES
+('Clean Code', 'Lerato Kgwedi', 'A Handbook of Agile Software Craftsmanship', 1),
+('The Pragmatic Programmer','Toufique Mahlangu', 'Your Journey to Mastery', 2),
+('Design Patterns', 'Oarabile Brown', 'Elements of Reusable Object-Oriented Software', 3),
+('Introduction to the Theory of Computation', 'Nkateko Green', 'Introduction to the theory of computation', 4),
+('Artificial Intelligence', 'Dineo CK', 'A Modern Approach', 5),
+('Structure and Interpretation of Computer Programs', 'Lerato Kgwedi', 'Scheme programming language. It', 1),
+('Code Complete', 'Toufique Mahlangu', 'A Practical Handbook of Software Construction', 2),
+('Operating System Concepts', 'Oarabile Brown', 'Fundamental concepts of operating systems,', 3),
+('Computer Networks', 'Nkateko Green', 'A detailed introduction to computer networks', 4),
+('Introduction to Algorithms', 'Dineo CK', 'Cide range of algorithms, including sorting, searching, graph algorithms, and dynamic programming', 5),
+('JS: ES6 & Beyond', 'Lerato Kgwedi', 'The latest version of the JavaScript language', 1),
+('Python Crash Course', 'Toufique Mahlangu', 'A Hands-On, Project-Based Introduction to Programming;', 2),
+('Deep Learning', 'Oarabile Brown', 'neural networks, optimization algorithms, and various deep learning architectures.', 3), 
+('The Art of Computer Programming', 'Nkateko Green', 'Covers a wide range of algorithms and data structure', 4),
+('Eloquent JavaScript', 'Toufique Mahlangu', 'Focus on writing elegant and efficient code. ', 2);
+
+
 SELECT COUNT(*) AS total_books
 FROM Books;
 
